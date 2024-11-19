290c80d72f04e231d924fde44ccf69669ce389e0
Kauanrodrigues01 <kauanrl09@gmail.com>
2024-11-19 17:00:05 -0300

    feat: adicionando mdoel de DailyReport

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -17,3 +17,20 @@ class ActivityLog(models.Model):
 
     def __str__(self):
         return f"{self.get_event_type_display()} - {self.description}"
+
+
+class DailyReport(models.Model):
+    date = models.DateField(unique=True)
+    active_students = models.PositiveIntegerField(default=0)
+    pending_students = models.PositiveIntegerField(default=0)
+    new_students = models.PositiveIntegerField(default=0)
+    daily_profit = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    
+    def __str__(self):
+        return f"Daily Report for {self.date}"
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['date']),
+        ]
+    
