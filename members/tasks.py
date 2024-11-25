eed3c5e27c626362312ea16641f83ba3886c80ab
Kauan Rodrigues Lima <162514268+Kauanrodrigues01@users.noreply.github.com>
2024-11-25 00:17:15 -0300

    Update tasks.py

--- members/tasks.py
+++ members/tasks.py
@@ -5,6 +5,7 @@ from .models import Member
 def update_members_activity_status():
     """Verifica se o pagamento do membro foi feito há mais de 1 mês e atualiza o status."""
     members = Member.objects.filter(is_active=True)
+    print('FOI CHAMADO A TASK DE ATUALIZAR MEMBROS')
 
     for member in members:
         member.update_activity_status()
