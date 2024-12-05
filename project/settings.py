ebbe7bab1600e038208a350550242a5241a8da8c
Kauan Rodrigues Lima <kauanrl09@gmail.com>
2024-12-05 09:48:18 -0300

    refactor: refatorando

--- project/settings.py
+++ project/settings.py
@@ -162,9 +162,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'testandoemail1100@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST', cast=str)
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', cast=str)  # Use uma senha de app se a autenticação de dois fatores estiver ativada
-DEFAULT_FROM_EMAIL = 'testandoemail1100@gmail.com'
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST', cast=str)
 
 # Celery settings
 CELERY_BROKER_URL = config('REDIS_URL', default='redis://localhost:6379/0')  # Endereço do Redis
